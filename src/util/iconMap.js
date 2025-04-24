fe23436e8a448205b1354156e4d7f16d4566ee14
[플랫폼]최다훈 <ekgns8258@douzone.com>
2025-04-24 13:02:53 +0900

    theventi Add

--- src/util/iconMap.js
+++ src/util/iconMap.js
@@ -2,12 +2,15 @@ import composeIcon from "../assets/icons/compose.png";
 import mcdonald from "../assets/icons/mcdonald.jpg";
 import megaCoffe from "../assets/icons/megaCoffe.png";
 import starbucks from "../assets/icons/starbucks.png";
+import theVenti from "../assets/icons/theVenti.png";
+
 
 export const keywordIconMap = {
   컴포즈: composeIcon,
   맥도날드: mcdonald,
   메가커피: megaCoffe,
   스타벅스: starbucks,
+  더벤티: theVenti,
   // 필요 시 추가
 };
 
